a4345f2b3c0ef3604eb96ef402b8b41113f1c8b2
Warshavski <p.warshavski@gmail.com>
2018-08-01 20:37:09 +0300

    Added : class(service) for composing information about shops

--- spec/factories/publisher.rb
+++ spec/factories/publisher.rb
@@ -2,4 +2,14 @@ FactoryBot.define do
   factory :publisher, class: Publisher do
     name { Faker::Book.publisher }
   end
+
+  factory :publisher_with_books, class: Publisher, parent: :publisher do
+    transient do
+      books_count 2
+    end
+
+    after(:create) do |publisher, evaluator|
+      create_list(:book, evaluator.books_count, publisher: publisher)
+    end
+  end
 end
