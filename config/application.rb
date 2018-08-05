acebb6d1ec35acd6834cbc16a15b2e9e9acfb93c
Warshavski <p.warshavski@gmail.com>
2018-08-05 17:31:47 +0300

    Added : the ability to search and sort publishers

--- config/application.rb
+++ config/application.rb
@@ -29,5 +29,7 @@ module BookStoreApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.autoload_paths += %W[#{config.root}/lib]
   end
 end
