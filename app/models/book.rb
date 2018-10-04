6d6ffba1568dd045c1a5b96c1b00badbe14ecb87
warshavski <p.warshavski@gmail.com>
2018-10-04 21:41:33 +0300

    Added: migration with detailed book info

--- app/models/book.rb
+++ app/models/book.rb
@@ -12,5 +12,10 @@ class Book < ApplicationRecord
 
   scope :in_stock, -> { joins(:stocks).where('stocks.quantity > 0').distinct }
 
-  validates :title, :publisher, presence: true
+  validates :title, :publisher, :published_at, presence: true
+
+  validates :isbn_10, length: { is: 10, allow_nil: true }
+  validates :isbn_13, length: { is: 13, allow_nil: true }
+
+  validates :isbn_10, :isbn_13, uniqueness: { allow_nil: true, case_sensitive: false }
 end
---
+++ db/migrate/20181004175259_add_books_detailed_columns.rb
@@ -0,0 +1,13 @@
+class AddBooksDetailedColumns < ActiveRecord::Migration[5.1]
+  def change
+    add_column :books, :isbn_13, :string, limit: 10
+    add_column :books, :isbn_10, :string, limit: 13
+    add_column :books, :published_at, :date_time, null: true
+
+    # Renew publish date for books that already was in table
+    date = Date.new(1111, 11, 11)
+    update "UPDATE books SET published_at = '#{date}'"
+
+    change_column_null :books, :published_at, false
+  end
+end
