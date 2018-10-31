49274dc369d0611acaaaf1d674c11982d6045554
warshavski <p.warshavski@gmail.com>
2018-10-31 10:55:36 +0300

    Refactored: exception_handler(some format fixes)

--- app/controllers/concerns/exception_handler.rb
+++ app/controllers/concerns/exception_handler.rb
@@ -11,38 +11,41 @@ module ExceptionHandler
     #
     rescue_from ActionController::ParameterMissing do |e|
       log_exception(e)
-      render json: { errors: [{ status: 400, detail: e.message, source: { pointer: e.param }}] },
-             status: :bad_request
+
+      render_errors([{ status: 400, detail: e.message, source: { pointer: e.param }}], :bad_request)
     end
 
     # Return 404 - Not Found
     #
     rescue_from ActiveRecord::RecordNotFound do |e|
       log_exception(e)
-      render json: { errors: [{ status: 404, detail: e.message}] },
-             status: :not_found
+
+      not_found(e.message)
     end
 
     # Return 422 - Unprocessable Entity (validation|duplicate record)
     #
     rescue_from ActiveRecord::RecordInvalid, ActiveRecord::RecordNotUnique do |e|
       log_exception(e)
-      render_error(e.record, :unprocessable_entity)
+
+      render_errors(ErrorSerializer.serialize(e.record, 422), :unprocessable_entity)
     end
 
     def not_found(message = 'Record not found')
-      render json: { errors: [{status: 404, detail: message }] },
-             status: :not_found
+      render_errors([{status: 404, detail: message }], :not_found)
     end
 
     private
 
-    def render_error(object, status)
-      render json: { errors: ErrorSerializer.serialize(object, status) }, status: status
+    def render_errors(object, status)
+      render json: { errors: object }, status: status
     end
 
     def log_exception(exception)
-      application_trace = ActionDispatch::ExceptionWrapper.new(ActiveSupport::BacktraceCleaner.new, exception).application_trace
+      application_trace = ActionDispatch::ExceptionWrapper
+                            .new(ActiveSupport::BacktraceCleaner.new, exception)
+                            .application_trace
+
       application_trace.map! { |t| "  #{t}\n" }
 
       logger.error "\n#{exception.class.name} (#{exception.message}):\n#{application_trace.join}"
