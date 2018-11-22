54be32eb1f42ce535d27752f145b038bffd25263
warshavski <p.warshavski@gmail.com>
2018-11-22 15:38:48 +0300

    Added: comments for default filters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,6 +11,20 @@ class ApplicationController < ActionController::API
     record.nil? ? not_found : yield(record)
   end
 
+  # Optional query parameters :
+  #
+  #   - search  filter by given term(pattern)
+  #               example: ?search=term
+  #
+  #   - sort    sort by attribute and direction
+  #               example: ?sort=name_asc
+  #
+  #   - page    filter by page number
+  #               example: ?page=1
+  #
+  #   - limit   limit items in collection
+  #               example: ?limit=1
+  #
   def filter_params
     params.permit(*(%i[search sort page limit] + specific_filters))
   end
