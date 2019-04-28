aa8fbf14a6dcfdd382dbb18cd1e6abc842b07d79
warshavski <p.warshavski@gmail.com>
2019-04-28 17:04:42 +0300

    Change class methods definition

--- lib/booky.rb
+++ lib/booky.rb
@@ -5,38 +5,39 @@
 #   Used for the core app settings
 #
 #     - version | revision
-#     - ? settings ?
+#     - settings entry point
 #
 module Booky
+  class << self
+    def root
+      Pathname.new(File.expand_path('..', __dir__))
+    end
 
-  def self.root
-    Pathname.new(File.expand_path('..', __dir__))
-  end
+    def config
+      Settings
+    end
 
-  def self.config
-    Settings
-  end
+    def migrations_hash
+      @_migrations_hash ||= Digest::MD5.hexdigest(ActiveRecord::Migrator.get_all_versions.to_s)
+    end
 
-  def self.migrations_hash
-    @_migrations_hash ||= Digest::MD5.hexdigest(ActiveRecord::Migrator.get_all_versions.to_s)
-  end
+    def revision
+      path = root.join('REVISION')
 
-  def self.revision
-    path = root.join('REVISION')
-
-    @_revision ||=
-      if File.exist?(path)
-        begin
-          File.read(path).strip.freeze
-        rescue EOFError
+      @_revision ||=
+        if File.exist?(path)
+          begin
+            File.read(path).strip.freeze
+          rescue EOFError
+            'Unknown'
+          end
+        else
           'Unknown'
         end
-      else
-        'Unknown'
-      end
-  end
+    end
 
-  def self.version
-    File.read(root.join('VERSION')).strip.freeze
+    def version
+      File.read(root.join('VERSION')).strip.freeze
+    end
   end
 end
